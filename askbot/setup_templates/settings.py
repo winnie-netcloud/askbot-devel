4518e5711fba46f18e519e70783e61f2ee924485
Martin BTS <martinbts@gmx.net>
2019-06-25 22:08:41 +0200

    BREAKING: Bump celery and kombu to 4.x
    * kombu 3.x
      - does not support Django 2.x
    * kombu 4.x
      - does support Django 2.x
      - requires celery 4.x
    * kombu 4.x
      - dropped the database transport Askbot has been using
    * changed settings.py to use kombu.transport.memory
      - meant for unit testing only, according to the fine manual

--- askbot/setup_templates/settings.py
+++ askbot/setup_templates/settings.py
@@ -156,7 +156,7 @@ INSTALLED_APPS = (
     'robots',
     'django_countries',
     'djcelery',
-    'kombu.transport.django',
+    'kombu.transport.memory',
     'followit',
     'tinymce',
     #'avatar',#experimental use git clone git://github.com/ericflo/django-avatar.git$
@@ -213,7 +213,7 @@ LOGIN_REDIRECT_URL = ASKBOT_URL #adjust if needed
 ALLOW_UNICODE_SLUGS = False
 
 #Celery Settings
-BROKER_TRANSPORT = "kombu.transport.django.Transport"
+BROKER_TRANSPORT = "kombu.transport.memory.Transport"
 CELERY_ALWAYS_EAGER = True
 
 import djcelery
