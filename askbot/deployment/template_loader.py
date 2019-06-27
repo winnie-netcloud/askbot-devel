6f5e531420b736f52e7cdb0ecdbb6c31a089b8b4
Martin BTS <martinbts@gmx.net>
2019-06-27 19:03:57 +0200

    Replace pystache with Jinja2
    * Drop in replacement

--- askbot/deployment/template_loader.py
+++ askbot/deployment/template_loader.py
@@ -1,13 +1,21 @@
 import os
-import pystache
+from copy import deepcopy
+from jinja2 import Template as Jinja2Template
 
-SOURCE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-class SettingsTemplate(pystache.View):
-    '''Class for settings'''
 
-    template_path = os.path.join(SOURCE_DIR, 'setup_templates')
-    template_name = "settings.py"
 
-    def __init__(self, context, **kwargs):
-        super(SettingsTemplate, self).__init__(context=context, **kwargs)
+class SettingsTemplate(object):
+    tmpl_dir  = 'setup_templates'
+    tmpl_name = 'settings.py.jinja2'
+
+    def __init__(self, context={}):
+        self.context = context
+        source_dir = os.path.dirname(os.path.dirname(__file__))
+        self.tmpl_path = os.path.join(source_dir,self.tmpl_dir,self.tmpl_name)
+
+    def render(self, param_context={}):
+        this_context = deepcopy(self.context)
+        this_context.update(param_context)
+        with open(self.tmpl_path, 'r') as template_file:
+            return Jinja2Template(template_file.read()).render(this_context)
