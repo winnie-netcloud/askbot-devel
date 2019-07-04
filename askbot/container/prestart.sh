16e17a828b7bb5e141385a9c86026ba9eedfd98d
Martin BTS <martinbts@gmx.net>
2019-07-04 15:11:22 +0200

    Make container startup more robust
    * do not fail if DB already exists
    * do not fail if admin already exists
    * do not fail if user was already made admin
    * allow for cron binary be named cron or crond
    * use absolut path for cron environment

--- askbot/container/prestart.sh
+++ askbot/container/prestart.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-python manage.py migrate --noinput
+python manage.py migrate --noinput || true
 if [ -n "$ADMIN_PASSWORD" ]
 then
-  python manage.py askbot_add_user --user-name admin --email admin@localhost --password "$ADMIN_PASSWORD" --status d
-  yes yes | python manage.py add_admin 1
+  python manage.py askbot_add_user --user-name admin --email admin@localhost --password "$ADMIN_PASSWORD" --status d ||
+true
+   { yes yes | python manage.py add_admin 1; } || true
 fi
 
 if [ -z "$NO_CRON" ]
 then
-    set | grep -e 'PATH' -e 'DATABASE_URL' -e 'SECRET_KEY' -e 'PYTHONUNBUFFERED' > cron_environ
-    cron
+    set | grep -e 'PATH' -e 'DATABASE_URL' -e 'SECRET_KEY' -e 'PYTHONUNBUFFERED' > /cron_environ
+    crond || cron
 fi
 
