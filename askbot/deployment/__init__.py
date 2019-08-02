32f3796834ed55f942bf7e404a88a4a26a3e5b65
Martin BTS <martinbts@gmx.net>
2019-08-02 23:24:10 +0200

    Use the app-name parameter, make --force defunct and housekeeping

--- askbot/deployment/__init__.py
+++ askbot/deployment/__init__.py
@@ -4,25 +4,21 @@ module for deploying askbot
 
 import os.path
 import sys
-from collections import OrderedDict
-from optparse import OptionParser
+
 from argparse import ArgumentParser
 from askbot.deployment import messages
 from askbot.deployment.messages import print_message
 from askbot.deployment import path_utils
-from askbot.utils import console
 from askbot.utils.functions import generate_random_key
 from askbot.deployment.template_loader import DeploymentTemplate
 from askbot.deployment.parameters import ConfigManagerCollection
 
 import shutil
 
-DATABASE_ENGINE_CHOICES = ('1', '2', '3', '4')
-
 class AskbotSetup:
 
-    PROJECT_FILES_TO_CREATE = set(('manage.py',))
-    APP_FILES_TO_CREATE     = set(path_utils.FILES_TO_CREATE) - set(('manage.py',))
+    PROJECT_FILES_TO_CREATE = {'manage.py'}
+    APP_FILES_TO_CREATE     = set(path_utils.FILES_TO_CREATE) - {'manage.py'}
     SOURCE_DIR              = os.path.dirname(os.path.dirname(__file__)) # a.k.a. ASKBOT_ROOT in settings.py
 
     def __init__(self, interactive=True, verbosity=-128):
@@ -118,7 +114,7 @@ class AskbotSetup:
             "--force",
             dest="force",
             action='store_true',
-            help="Force overwrite settings.py file"
+            help="(DEFUNCT!) Force overwrite settings.py file"
         )
 
         self.parser.add_argument(
@@ -262,6 +258,7 @@ class AskbotSetup:
       try:
         options = self.parser.parse_args()
         options = vars(options)
+        options['force'] = False # disable the --force switch!
         self._set_verbosity(options['verbosity'])
         self._set_create_project(options)
         print_message(messages.DEPLOY_PREAMBLE, self.verbosity)
@@ -353,10 +350,10 @@ class AskbotSetup:
         path_utils.touch(log_file)
         self._install_copy(copy_me, skip_silently=path_utils.BLANK_FILES)
 
-    def _create_new_django_app(self, app_name, options):
-        options['askbot_site'] = options['dir_name']
-        options['askbot_app']  = app_name
-        app_dir =  os.path.join(options['dir_name'], app_name)
+    def _create_new_django_app(self, options):
+        options['askbot_site'] = options['dir_name'] # b/c the jinja template uses askbot_site
+        options['askbot_app']  = options['app_name'] # b/c the jinja template uses askbot_app
+        app_dir = os.path.join(options['dir_name'], options['app_name'])
 
         create_me = [ app_dir ]
         copy_me   = list()
@@ -415,13 +412,13 @@ class AskbotSetup:
            options['force'] is False:
              create_new_project = False
 
-        options['staticfiles_app'] = "'django.contrib.staticfiles',"
-        options['auth_context_processor'] = 'django.contrib.auth.context_processors.auth'
+        options['staticfiles_app'] = "'django.contrib.staticfiles'," # Fixme: move this into the template
+        options['auth_context_processor'] = 'django.contrib.auth.context_processors.auth' # Fixme: move this into the template
 
         if create_new_project is True:
             self._create_new_django_project(options['dir_name'], options)
 
-        self._create_new_django_app('askbot_app', options)
+        self._create_new_django_app(options)
 
         help_file = path_utils.get_path_to_help_file()
 
@@ -438,243 +435,3 @@ class AskbotSetup:
 
 # set to askbot_setup_orig to return to original installer
 askbot_setup = AskbotSetup(interactive=True, verbosity=1)
-
-def askbot_setup_orig():
-    """basic deployment procedure
-    asks user several questions, then either creates
-    new deployment (in the case of new installation)
-    or gives hints on how to add askbot to an existing
-    Django project
-    """
-    parser = OptionParser(usage = "%prog [options]")
-
-    parser.add_option(
-                "-v", "--verbose",
-                dest = "verbosity",
-                type = "int",
-                default = 1,
-                help = "verbosity level available values 0, 1, 2."
-            )
-
-    parser.add_option(
-                "-n", "--dir-name",
-                dest = "dir_name",
-                default = None,
-                help = "Directory where you want to install."
-            )
-
-    parser.add_option(
-                '-e', '--db-engine',
-                dest='database_engine',
-                action='store',
-                type='choice',
-                choices=DATABASE_ENGINE_CHOICES,
-                default=None,
-                help='Database engine, type 1 for postgresql, 2 for sqlite, 3 for mysql'
-            )
-
-    parser.add_option(
-                "-d", "--db-name",
-                dest = "database_name",
-                default = None,
-                help = "The database name"
-            )
-
-    parser.add_option(
-                "-u", "--db-user",
-                dest = "database_user",
-                default = None,
-                help = "The database user"
-            )
-
-    parser.add_option(
-                "-p", "--db-password",
-                dest = "database_password",
-                default = None,
-                help = "the database password"
-            )
-
-    parser.add_option(
-                "--db-host",
-                dest = "database_host",
-                default = None,
-                help = "the database host"
-            )
-
-    parser.add_option(
-                "--db-port",
-                dest = "database_port",
-                default = None,
-                help = "the database host"
-            )
-
-    parser.add_option(
-                "--logfile-name",
-                dest="logfile_name",
-                default='askbot.log',
-                help="name of the askbot logfile."
-            )
-
-    parser.add_option(
-                "--append-settings",
-                dest = "local_settings",
-                default = '',
-                help = "Extra settings file to append custom settings"
-            )
-
-    parser.add_option(
-                "--force",
-                dest="force",
-                action='store_true',
-                default=False,
-                help = "Force overwrite settings.py file"
-            )
-    parser.add_option(
-                "--no-secret-key",
-                dest="no_secret_key",
-                action='store_true',
-                default=False,
-                help="Don't generate a secret key. (not recommended)"
-            )
-
-    try:
-        options = parser.parse_args()[0]
-
-        #ask users to give missing parameters
-        #todo: make this more explicit here
-        if options.verbosity >= 1:
-            print(messages.DEPLOY_PREAMBLE)
-
-        directory = path_utils.clean_directory(options.dir_name)
-        while directory is None:
-            directory = path_utils.get_install_directory(force=options.force)
-        options.dir_name = directory
-
-        if options.database_engine not in DATABASE_ENGINE_CHOICES:
-            options.database_engine = console.choice_dialog(
-                'Please select database engine:\n1 - for postgresql, '
-                '2 - for sqlite, 3 - for mysql, 4 - oracle',
-                choices=DATABASE_ENGINE_CHOICES
-            )
-
-        options_dict = vars(options)
-        if options.force is False:
-            options_dict = collect_missing_options(options_dict)
-
-        database_engine_codes = {
-            '1': 'postgresql_psycopg2',
-            '2': 'sqlite3',
-            '3': 'mysql',
-            '4': 'oracle'
-        }
-        database_engine = database_engine_codes[options.database_engine]
-        options_dict['database_engine'] = database_engine
-
-        deploy_askbot(options_dict)
-
-        if database_engine == 'postgresql_psycopg2':
-            try:
-                import psycopg2
-            except ImportError:
-                print('\nNEXT STEPS: install python binding for postgresql')
-                print('pip install psycopg2\n')
-        elif database_engine == 'mysql':
-            try:
-                import _mysql
-            except ImportError:
-                print('\nNEXT STEP: install python binding for mysql')
-                print('pip install mysql-python\n')
-
-    except KeyboardInterrupt:
-        print("\n\nAborted.")
-        sys.exit(1)
-
-
-#separated all the directory creation process to make it more useful
-def deploy_askbot(options):
-    """function that creates django project files,
-    all the neccessary directories for askbot,
-    and the log file
-    """
-    create_new_project = True
-    if os.path.exists(options['dir_name']):
-        if path_utils.has_existing_django_project(options['dir_name']):
-            create_new_project = bool(options['force'])
-
-    path_utils.create_path(options['dir_name'])
-
-    options['staticfiles_app'] = "'django.contrib.staticfiles',"
-
-    options['auth_context_processor'] = 'django.contrib.auth.context_processors.auth'
-
-    verbosity = options['verbosity']
-
-    path_utils.deploy_into(
-        options['dir_name'],
-        new_project=create_new_project,
-        verbosity=verbosity,
-        context=options
-    )
-
-    help_file = path_utils.get_path_to_help_file()
-
-    if create_new_project:
-        print_message(
-            messages.HOW_TO_DEPLOY_NEW % {'help_file': help_file},
-            verbosity
-        )
-    else:
-        print_message(
-            messages.HOW_TO_ADD_ASKBOT_TO_DJANGO % {'help_file': help_file},
-            verbosity
-        )
-
-def collect_missing_options(options_dict):
-    options_dict['secret_key'] = '' if options_dict['no_secret_key'] else generate_random_key()
-    if options_dict['database_engine'] == '2':#sqlite
-        if options_dict['database_name']:
-            return options_dict
-        while True:
-            value = console.simple_dialog(
-                            'Please enter database file name'
-                        )
-            database_file_name = None
-            if os.path.isfile(value):
-                message = 'file %s exists, use it anyway?' % value
-                if console.get_yes_or_no(message) == 'yes':
-                    database_file_name = value
-            elif os.path.isdir(value):
-                print('%s is a directory, choose another name' % value)
-            elif value in path_utils.FILES_TO_CREATE:
-                print('name %s cannot be used for the database name' % value)
-            elif value == path_utils.LOG_DIR_NAME:
-                print('name %s cannot be used for the database name' % value)
-            else:
-                database_file_name = value
-
-            if database_file_name:
-                options_dict['database_name'] = database_file_name
-                return options_dict
-
-    else:#others
-        db_keys = OrderedDict([
-            ('database_name', True),
-            ('database_user', True),
-            ('database_password', True),
-            ('database_host', False),
-            ('database_port', False)
-        ])
-        for key, required in list(db_keys.items()):
-            if options_dict[key] is None:
-                key_name = key.replace('_', ' ')
-                fmt_string = '\nPlease enter %s'
-                if not required:
-                    fmt_string += ' (press "Enter" to use the default value)'
-
-                value = console.simple_dialog(
-                    fmt_string % key_name,
-                    required=db_keys[key]
-                )
-
-                options_dict[key] = value
-        return options_dict
