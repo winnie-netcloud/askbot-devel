bd08fe639c9bd33b5351e9434c9c2630a1e1a50b
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2020-01-18 13:30:14 -0300

    1) updates pytz version 2) models.ForeignKey(unique=True) -> models.OneToOneField()

--- askbot/deps/group_messaging/models.py
+++ askbot/deps/group_messaging/models.py
@@ -99,7 +99,7 @@ class SenderList(models.Model):
     sender list is populated automatically
     as new messages are created
     """
-    recipient = models.ForeignKey(Group, unique=True)
+    recipient = models.OneToOneField(Group)
     senders = models.ManyToManyField(User)
     objects = SenderListManager()
 
