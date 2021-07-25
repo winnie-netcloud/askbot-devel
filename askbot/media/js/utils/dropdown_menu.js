1e99a69e97c2bedba2def2f361d59a914d5c1aa2
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2021-07-25 15:02:51 -0400

    WIP #889: * fully resposive meta-nav * DropdownMenu - can be disabled/enabled dynamically * split sass/component/headers.scss to create meta_nav.scss and user_nav.scss

--- askbot/media/js/utils/dropdown_menu.js
+++ askbot/media/js/utils/dropdown_menu.js
@@ -1,23 +1,38 @@
+var DropdownMenu = function () {};
+
+DropdownMenu.prototype.decorate = function(elem) {
+  var elem = $(elem);
+  if (elem.hasClass('js-mounted')) { /* no remounts */
+    return
+  }
+
+  var target = elem.find('.js-dropdown-target');
+
+  function toggleMenu (evt) {
+    evt.stopPropagation();
+    if (!elem.hasClass('js-dropdown-menu')) {/* allow disabling the menu dynamically by removing this class */
+      return
+    }
+    if (target.hasClass('js-dropdown-active')) {
+      target.removeClass('js-dropdown-active');
+    } else {
+      target.addClass('js-dropdown-active');
+    }
+  }
+
+  $(elem).click(function(evt) {
+    evt.stopPropagation();
+  });
+
+  var trigger = $(elem).find('.js-dropdown-trigger');
+  trigger.click(toggleMenu);
+  $(window).click(function () { target.removeClass('js-dropdown-active') });
+  elem.addClass('js-mounted');
+};
+
 (function() {
   $('.js-dropdown-menu').each(function(_, elem) {
-
-    var target = $(elem).find('.js-dropdown-target');
-
-    function toggleMenu (evt) {
-      evt.stopPropagation();
-      if (target.hasClass('js-dropdown-active')) {
-        target.removeClass('js-dropdown-active');
-      } else {
-        target.addClass('js-dropdown-active');
-      }
-    }
-
-    $(elem).click(function(evt) {
-      evt.stopPropagation();
-    });
-
-    var trigger = $(elem).find('.js-dropdown-trigger');
-    trigger.click(toggleMenu);
-    $(window).click(function () { target.removeClass('js-dropdown-active') });
+    var menu = new DropdownMenu();
+    menu.decorate($(elem));
   })
 })();
