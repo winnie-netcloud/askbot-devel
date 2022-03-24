bd9d0f27f416c5b24fa9ce1874a505d7c5f08b62
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2022-03-24 14:44:23 -0300

    updates dependency django-livesettings3==1.5.0

--- askbot/__init__.py
+++ askbot/__init__.py
@@ -29,7 +29,7 @@ REQUIREMENTS = {
     'jsonfield': 'jsonfield>=2.0.0',
     'jwt': 'pyjwt<=1.7.1',
     'keyedcache': 'django-keyedcache3>=1.5.1',
-    'livesettings': 'django-livesettings3==1.4.21',
+    'livesettings': 'django-livesettings3==1.5.0',
     'markdown2': 'markdown2<=2.3.9',
     'mock': 'mock==3.0.5',
     'oauth2': 'oauth2<=1.9.0.post1',
